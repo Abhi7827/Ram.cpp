382dee9fc1c160976939f4c8a50f00ff5ca86eb7
Abhi7827 <103488518+Abhi7827@users.noreply.github.com>
2022-10-28 08:45:35 +0530

    Add files via upload

---
+++ Arshdeep.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1, num2;
+    cout << "Input the values for Number1 and Number2 :";
+    cin >> num1 >> num2;
+    if (num1 == num2)
+    {
+        cout << "Number1 and Number2 are equal";
+    }
+    else
+    {
+        cout << "Number1 and Number2 are not equal";
+    }
+}
+
